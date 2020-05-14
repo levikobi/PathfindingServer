b2de344f0ed779c4e242dc27c122182015af26c2
levikobi <56400138+levikobi@users.noreply.github.com>
2020-05-14 16:27:56 +0300

    add server_side\MySerialServer

---
+++ src/server_side/MySerialServer.java
@@ -0,0 +1,13 @@
+package server_side;
+
+public class MySerialServer implements Server {
+    @Override
+    public void open(int port, ClientHandler c) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+    }
+}
