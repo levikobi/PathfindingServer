3df93a8bb10e87ea1aecfda9e3746ed959c7bc9b
levikobi <56400138+levikobi@users.noreply.github.com>
2020-05-14 16:34:58 +0300

    add server_side\FileCacheManager

---
+++ src/server_side/FileCacheManager.java
@@ -0,0 +1,26 @@
+package server_side;
+
+public class FileCacheManager implements CacheManager {
+
+	public FileCacheManager() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+//    @Override
+//    public boolean isCached(Problem problem) {
+//        // TODO Auto-generated method stub
+//        return false;
+//    }
+//
+//    @Override
+//    public Solution loadSolution(Problem problem) {
+//        // TODO Auto-generated method stub
+//        return null;
+//    }
+//
+//    @Override
+//    public void saveSolution(Problem problem, Solution solution) {
+//        // TODO Auto-generated method stub
+//    }
+}
