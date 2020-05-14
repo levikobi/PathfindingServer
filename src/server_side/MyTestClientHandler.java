12f09770b150d1e206c3ff242ea4177fc6e73665
levikobi <56400138+levikobi@users.noreply.github.com>
2020-05-14 16:33:04 +0300

    add server_side\MyTestClientHandler

---
+++ src/server_side/MyTestClientHandler.java
@@ -0,0 +1,14 @@
+package server_side;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MyTestClientHandler implements ClientHandler {
+    private Solver solver;
+    private CacheManager cm;
+
+    @Override
+    public void handleClient(InputStream inputStream, OutputStream outputStream) {
+        // TODO Auto-generated method stub
+    }
+}
