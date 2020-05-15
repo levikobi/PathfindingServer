b60ea4a712bd86c8b3ccf99a344523c5ef7d3246
levikobi <56400138+levikobi@users.noreply.github.com>
2020-05-15 14:17:36 +0300

    mod MySerialServer: open

--- src/server_side/MySerialServer.java
+++ src/server_side/MySerialServer.java
@@ -1,9 +1,29 @@
 package server_side;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 public class MySerialServer implements Server {
     @Override
-    public void open(int port, ClientHandler c) {
-        // TODO Auto-generated method stub
+    public void open(int port, ClientHandler c) throws SocketTimeoutException {
+        ServerSocket server = new ServerSocket(port);
+        server.setSoTimeout(1000);
+        try {
+            Socket aClient = server.accept();
+
+            InputStream inFromClient = aClient.getInputStream();
+            OutputStream outToClient = aClient.getOutputStream();
+
+            //
+
+            inFromClient.close();
+            outToClient.close();
+            aClient.close();
+            server.close();
+        } catch (SocketTimeoutException e) { e.printStackTrace(); }
     }
 
     @Override
--- src/server_side/Server.java
+++ src/server_side/Server.java
@@ -1,6 +1,8 @@
 package server_side;
 
+import java.net.SocketTimeoutException;
+
 public interface Server {
-    public void open(int port, ClientHandler c);
+    public void open(int port, ClientHandler c) throws SocketTimeoutException;
     public void stop();
 }
