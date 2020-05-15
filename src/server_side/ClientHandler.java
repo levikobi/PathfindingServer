dfc8527420f755d85b2c9a1795189161b1493788
levikobi <56400138+levikobi@users.noreply.github.com>
2020-05-15 14:20:10 +0300

    mod ClientHandler

--- src/server_side/ClientHandler.java
+++ src/server_side/ClientHandler.java
@@ -4,5 +4,5 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public interface ClientHandler {
-    public void handleClient(InputStream inputStream, OutputStream outputStream);
+    public void handleClient(InputStream inFromClient, OutputStream outToClient);
 }
