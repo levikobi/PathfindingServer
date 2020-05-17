c62dda30f553c4ebd2c8b388f56d2d45f901cab0
levikobi <56400138+levikobi@users.noreply.github.com>
2020-05-17 18:58:22 +0300

    implement State class

--- src/server_side/State.java
+++ src/server_side/State.java
@@ -1,5 +1,31 @@
 package server_side;
 
-public class State {
+public class State<T> {
+    private T state;
+    private double cost;       // cost to reach this state
+    private State<T> cameFrom; // the state we came from to this state
 
+    public State(T state) {
+        this.state = state;
+    }
+
+    public boolean equals(State<T> s) {
+        return state.equals(s.state);
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+//    public static void main(String[] args) {
+//        State<String> a, b, goal;
+//        a = new State<>("A");
+//        b = new State<>("B");
+//        goal = new State<>("B");
+//        System.out.println(b.equals(goal));
+//    }
 }
